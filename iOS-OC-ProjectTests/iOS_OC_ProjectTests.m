89c664f373b6b8c9b3979309621eb340c220b70e
jiliuliu <liujiliu1989@163.com>
2018-03-19 18:58:23 +0800

    Initial Commit

---
+++ iOS-OC-ProjectTests/iOS_OC_ProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  iOS_OC_ProjectTests.m
+//  iOS-OC-ProjectTests
+//
+//  Created by 刘吉六 on 2018/3/19.
+//  Copyright © 2018年 liujiliu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS_OC_ProjectTests : XCTestCase
+
+@end
+
+@implementation iOS_OC_ProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
